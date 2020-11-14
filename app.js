7581e04149f1e7c382a75fddb92474d13921f419
Abdullah <abdullah.5121011@gmail.com>
2020-11-14 15:25:54 -0700

    Implemented Google passport strategy

--- app.js
+++ app.js
@@ -3,11 +3,18 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const exphbs = require('express-handlebars');
+const passport = require('passport');
+const session = require('express-session');
 const connectDB = require('./config/db');
+
 
 //load config
 dotenv.config({ path: './config/config.env' });
 
+//pasport config
+require('./config/passport')(passport)
+
+//database connection
 connectDB();
 
 const app = express();
@@ -21,6 +28,17 @@ if (process.env.NODE_ENV == 'production') {
 // Handlebars
 app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', '.hbs');
+
+//session middleware
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+}))
+
+//passport middileware
+app.use(passport.initialize());
+app.use(passport.session());
 
 //static folder
 app.use(express.static(path.join(__dirname, 'public')));
