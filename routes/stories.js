10190a30e5310b8a43ff67b3989faca8ca548c24
Abdullah <abdullah.5121011@gmail.com>
2020-11-22 12:15:01 -0700

    Implemented Edit and Delete stories

--- routes/stories.js
+++ routes/stories.js
@@ -7,7 +7,7 @@ const Story = require('../models/Story');
 // @desc Show Add page
 // @route GET /stories/add
 router.get('/add', ensureAuth, (req, res) => {
-    res.render('stories/add',);
+    res.render('stories/add');
 })
 
 
@@ -36,5 +36,63 @@ router.get('/', ensureAuth, async (req, res) => {
         res.render('error/500');
     }
 })
+
+// @desc Show Edit Page
+// @route GET /stories/edit/:id
+router.get('/edit/:id', ensureAuth, async (req, res) => {
+    try {
+        const story = await Story.findOne({ _id: req.params.id }).lean();
+        if (!story) {
+            return res.render('error/404');        
+        }
+        
+        if (story.user != req.user.id){
+            res.redirect('/stories');
+        }else {
+            res.render('stories/edit', { story });
+        }
+    }catch (e) {
+        console.error(e);
+        return res.render('error/500');
+    }
+});
+
+// @desc update story
+// @route PUT /stories/:id
+router.put('/:id', ensureAuth, async (req, res) => {
+    try{
+        let story = await Story.findById(req.params.id).lean();
+
+        if(!story) {
+            return res.render('error/400');
+        }
+
+        if (story.user != req.user.id){
+            res.redirect('/stories');
+        } else {
+            story = await Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
+                new: true,
+                runValidators: true
+            });
+            res.redirect('/dashboard');
+        }
+    } catch (e) {
+        console.error(e);
+        return res.render('error/500');
+    }
+});
+
+// @desc delete story
+// @route DELETE /stories/:id
+router.delete('/:id', ensureAuth, async (req, res) => {
+    try{
+        await Story.remove({ _id: req.params.id });
+        res.redirect('/dashboard');
+    }catch (e) {
+        console.error(e);
+        return res.render('error/500');
+    }
+    
+});
 
 module.exports = router;
